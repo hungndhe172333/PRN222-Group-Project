689caeb67781137f538c8296e197a885d9a902d1
HungND <fctuoiconde@gmail.com>
2025-02-02 23:36:10 +0700

    update

--- 14_GroupProject_PRN222_SE1810/obj/Debug/net8.0/14_GroupProject_PRN222_SE1810.AssemblyInfo.cs
+++ 14_GroupProject_PRN222_SE1810/obj/Debug/net8.0/14_GroupProject_PRN222_SE1810.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("14_GroupProject_PRN222_SE1810")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d5e142bbe72a7c26d9bbb6150d7336d84f2c9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a4ab7d27830b467241b8cdcd9741f12d37a3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("14_GroupProject_PRN222_SE1810")]
 [assembly: System.Reflection.AssemblyTitleAttribute("14_GroupProject_PRN222_SE1810")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
