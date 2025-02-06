d071ca594471581e4af7a32fb8f49b0f0faff6a7
HungND <fctuoiconde@gmail.com>
2025-02-06 10:14:52 +0700

    MVC

---
+++ 14_PRN222_SE1810/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _14_PRN222_SE1810.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
