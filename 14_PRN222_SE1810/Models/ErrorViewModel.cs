dee403e57edd6c28fc3be497f0a2696bf48c3842
HungND <fctuoiconde@gmail.com>
2025-02-19 00:00:59 +0700

    delete

--- 14_PRN222_SE1810/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _14_PRN222_SE1810.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
