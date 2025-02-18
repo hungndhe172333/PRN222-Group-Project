9a2ac143f86117c4bb5efd583c7d83eb517f2ca8
HungND <fctuoiconde@gmail.com>
2025-02-18 23:38:08 +0700

    first push

---
+++ PRN222_Team14/Models/Brand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElectronicShopTeam14.Models;
+
+public partial class Brand
+{
+    public int BrandId { get; set; }
+
+    public string BrandName { get; set; } = null!;
+
+    public string? BrandDescription { get; set; }
+
+    public string? BrandCountry { get; set; }
+
+    public string? BrandLogo { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
