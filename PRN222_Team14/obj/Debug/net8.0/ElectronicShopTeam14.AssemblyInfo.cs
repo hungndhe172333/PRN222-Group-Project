d22bdf5eeb348de0bad7895c046daa8e7b79b4d9
DoanhTran <doanhtnhe172637@fpt.edu.vn>
2025-03-04 22:56:56 +0700

    Update bill

--- PRN222_Team14/obj/Debug/net8.0/ElectronicShopTeam14.AssemblyInfo.cs
+++ PRN222_Team14/obj/Debug/net8.0/ElectronicShopTeam14.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectronicShopTeam14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb91bb4be4ace022df38a1e758b685d2496985db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215e8363d9c7b3dd48b55db291b9309189f73320")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectronicShopTeam14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectronicShopTeam14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
